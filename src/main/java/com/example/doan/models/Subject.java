3f812febc52a65d72e2c21d9aefe55c69045612d
duong.hoangdang <duong.hoangdang@vti.com.vn>
2023-04-12 08:46:36 +0700

    Revert "fix insert subject"
    This reverts commit 4493d9b27740b230bf02cacd3836e799d3bc096f.

--- src/main/java/com/example/doan/models/Subject.java
+++ src/main/java/com/example/doan/models/Subject.java
@@ -35,8 +35,8 @@ public class Subject {
 
     @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(name = "teacher_subject",
-            joinColumns = @JoinColumn(name = "id_subject"),
-            inverseJoinColumns = @JoinColumn(name = "id_teacher"))
+            joinColumns = @JoinColumn(name = "id_teacher"),
+            inverseJoinColumns = @JoinColumn(name = "id_subject"))
     @JsonIgnore
     private List<User> listTeacher;
 }
